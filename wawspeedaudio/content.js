e5405f3586fb0f5e74a29a29317e5b2a00cbcde3
Rafael WMS <rafael.wms@msn.com>
2020-04-20 21:30:25 -0300

    Versão 1.0.0

--- wawspeedaudio/content.js
+++ wawspeedaudio/content.js
@@ -1,26 +1,70 @@
 
+const speedsAllowed = [0.25, 0.5, 0.75, 1.0, 2.0, 3.0, 4.0];
+var speedsAllowedIndex = 3;
+var currentSpeed = speedsAllowed[speedsAllowedIndex];
+const buttonMinusSpeed = document.createElement("button")
+const buttonCurrentSpeed = document.createElement("button")
+const buttonMoreSpeed = document.createElement("button")
+
+function createButtons(){
+    
+    buttonMinusSpeed.innerHTML = "<<"
+    buttonMinusSpeed.title = "Diminuir Velocidade"
+    buttonMinusSpeed.classList.add("changeSpeedButton")
+    buttonMinusSpeed.addEventListener("click", () => {
+            if(speedsAllowedIndex > 0){
+                speedsAllowedIndex -= 1;
+                currentSpeed = speedsAllowed[speedsAllowedIndex]
+                changeCurrentSpeedLabel();
+                changeAudiosSpeed();
+            }
+    })
+
+    buttonCurrentSpeed.classList.add("currentSpeedButton")
+    buttonCurrentSpeed.title = "Velocidade Atual"
+    changeCurrentSpeedLabel();
+
+    
+    buttonMoreSpeed.innerHTML = ">>"
+    buttonMoreSpeed.title = "Aumentar Velocidade"
+    buttonMoreSpeed.classList.add("changeSpeedButton")
+    buttonMoreSpeed.addEventListener("click", () => {
+        if(speedsAllowedIndex < 6){
+            speedsAllowedIndex += 1;
+            currentSpeed = speedsAllowed[speedsAllowedIndex]
+            changeCurrentSpeedLabel();
+            changeAudiosSpeed();
+        }
+           
+    })
+
+
+}
+
+function changeCurrentSpeedLabel(){
+    buttonCurrentSpeed.innerHTML = currentSpeed + "x";
+}
+
+function changeAudiosSpeed(){
+    const audios = document.querySelectorAll("audio");
+            console.log(audios)
+            audios.forEach((audio) => {
+                console.log(audio)
+                audio.playbackRate = currentSpeed;
+            })
+}
+
 const interval = setInterval(()=>{
     const header = document.querySelector("._3auIg")
     if(header){
            console.log(header)
            clearInterval(interval)
            
-           const button = document.createElement("button")
-           button.innerHTML = "2x"
-           button.classList.add("twoTimesButton")
+           createButtons();
 
-           button.addEventListener("click", () => {
-            const audios = document.querySelectorAll("audio");
-            console.log(audios)
-            audios.forEach((audio) => {
-                console.log(audio)
-                audio.playbackRate = 2;
-            })
-           
-        })
-
-           header.appendChild(button)
-
+           header.appendChild(buttonMinusSpeed)
+           header.appendChild(buttonCurrentSpeed)
+           header.appendChild(buttonMoreSpeed)
 
     }
 }, 1000)
